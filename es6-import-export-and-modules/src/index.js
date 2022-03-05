5161c2ac06128f5cb26b93c24dfb26217d6b3410
Gauri P <gauri.be0811@gmail.com>
2022-03-06 03:02:31 +0530

    ES6 in React - Import, export & modules basics

---
+++ es6-import-export-and-modules/src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import PI, { doublePi, triplePi } from "./math";
+
+ReactDOM.render(
+  <ul>
+    <li>{PI}</li>
+    <li>{doublePi()}</li>
+    <li>{triplePi()}</li>
+  </ul>,
+  document.getElementById("root")
+);
