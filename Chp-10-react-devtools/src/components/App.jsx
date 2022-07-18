d81b2c6c71753648bf3fff257b0b02690b028012
Gauri P <gauri.be0811@gmail.com>
2022-07-18 19:36:36 +0530

    React Dev tools-  Contact App - some reafactoring, componentization

---
+++ Chp-10-react-devtools/src/components/App.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+import Avatar from "./Avatar";
+
+function createCard(contact) {
+  return (
+    <Card
+      id={contact.id}
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Avatar avatarImg="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwck8Q3ZL1Z_h05yn-lNdajJJoEUgORmjjLQlqwItzLGZ1t6t0pUG6-6aBrgGAVfAZpFw&usqp=CAU" />
+
+      {contacts.map(createCard)}
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
+    </div>
+  );
+}
+
+export default App;
