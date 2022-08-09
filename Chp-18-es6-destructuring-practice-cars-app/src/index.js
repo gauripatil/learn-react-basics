3762446987a4100bdf7ae1f2ac7b303988199164
Gauri P <gauri.be0811@gmail.com>
2022-08-09 14:57:35 +0530

    ES6 Destructuring practice - animals application

--- Chp-18-es6-destructuring-practice-cars-app/src/index.js
+++ Chp-18-es6-destructuring-practice-cars-app/src/index.js
@@ -1,3 +1,22 @@
+// import animals, { useAnimals } from "./data";
+
+// //Destructuring Arrays
+// // console.log(animals);
+// const [cat, dog] = animals;
+// // console.log(cat);
+
+// const [animal, makeSound] = useAnimals(cat);
+// console.log(animal);
+// makeSound();
+
+// //Destructuring Objects
+// // const { name, sound} = cat;
+// // const { name: catName, sound: catSound } = cat;
+// // const { name = "Fluffy", sound = "Purr" } = cat;
+// // const {feedingRequirements: {food, water} } = cat;
+// // console.log(food);
+
+
 // CHALLENGE: uncomment the code below and see the car stats rendered
 import React from "react";
 import ReactDOM from "react-dom";
